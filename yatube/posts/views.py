115c7801925fd5ef97e3e85eead1ac6aabbaac15
Anton Stukach <anton.stukach@gmail.com>
2021-11-03 19:59:59 +0300

    minor change

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Главная страница блога пока еще убога')
+    template = 'posts/index.html'
+    return render(request, template)
 
 def group_posts(request, group_name):
     return HttpResponse(f'А теперь посмотрим на посты группы {group_name}')
